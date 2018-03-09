af2e9a445563f8a388968dc8353af8a49d848c93
Xu Zhou <zhouxu@nudt.edu.cn>
2018-03-09 13:13:41 +0800

    disassemble example OK.

--- disassemble.py
+++ disassemble.py
@@ -5,7 +5,16 @@ ld = cle.Loader("/bin/ls")
 entry = ld.main_object.entry
 min_addr = ld.main_object.min_addr
 max_addr = ld.main_object.max_addr
-CODE = ''.join(ld.memory.read_bytes(ld.main_object.entry, 0x100))
+
+raw_bytes = ld.memory.read_bytes(ld.main_object.entry, max_addr-min_addr)
+CODE = ''.join( raw_bytes )
 md = Cs(CS_ARCH_X86, CS_MODE_64)
-7 for i in md.disasm(CODE, 0x1000):
-8     print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+for i in md.disasm(CODE, entry):
+    print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+
+print "len(raw_bytes) = ", len(raw_bytes)
+print "entry_point = ", hex(ld.main_object.entry)
+print "min_addr = ", hex(min_addr)
+print "max_addr = ", hex(max_addr)
+print "max_addr - min_addr = ", max_addr - min_addr
+print "loader min and max addr: ", ld.min_addr, ld.max_addr
