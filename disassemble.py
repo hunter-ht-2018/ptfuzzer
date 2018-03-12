573cad52522b0c089044335592b2a67135a7b4a9
luoyyqq <hesln@qq.com>
2018-03-12 16:26:24 +0800

    读取elf文件.text段数据至输出文件

--- disassemble.py
+++ disassemble.py
@@ -4,18 +4,45 @@ import cle
 from capstone import *
 import argparse
 
-parser = argparse.ArgumentParser(description='Process bin name.')
-parser.add_argument('string', type= str, help='bin name')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Process bin name.')
+# parser.add_argument('string', type= str, help='bin name')
+# args = parser.parse_args()
 
-ld = cle.Loader(args.string)
+# ld = cle.Loader(args.string)
+
+ld = cle.Loader("/home/johnny/ptfuzzer/afl-pt/ptest/readelf")
+
+f = open("./raw.txt", "wb")
+bin_code = ""
+
 entry = ld.main_object.entry
-min_addr = ld.main_object.min_addr
-max_addr = ld.main_object.max_addr
+# 'data', 'header', 'is_null', 'name', 'stream'
 
-f = open("./min_max.txt", "w")
-f.write(str(min_addr) + "\n" + str(max_addr) + "\n" + str(entry))
+sec = ld.main_object.reader.get_section_by_name(".text")
+print sec.header.sh_addr, sec.header.sh_size
+raw_bytes = ld.memory.read_bytes(sec.header.sh_addr, sec.header.sh_size)
+for byte in raw_bytes:
+	bin_code += byte
+
+# for i in ld.main_object.sections:
+# 	if i.name == ".text":
+# 		print i.vaddr, i.filesize
+# 		raw_bytes = ld.memory.read_bytes(i.vaddr, i.filesize)
+# 		for byte in raw_bytes:
+# 			bin_code += byte
+
+print len(bin_code)
+f.write(bin_code)
 f.close()
+
+# print hex(entry)
+
+# min_addr = ld.main_object.min_addr
+# max_addr = ld.main_object.max_addr
+
+# f = open("./min_max.txt", "w")
+# f.write(str(min_addr) + "\n" + str(max_addr) + "\n" + str(entry))
+# f.close()
 
 
 
