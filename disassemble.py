dcf4543b6c510be299b640e9fc08bab90499832a
zhanggen <zhanggenex@qq.com>
2018-03-13 08:33:55 +0800

    读取代码段地址范围存入min_max.txt

--- disassemble.py
+++ disassemble.py
@@ -4,25 +4,19 @@ import cle
 from capstone import *
 import argparse
 
-# parser = argparse.ArgumentParser(description='Process bin name.')
-# parser.add_argument('string', type= str, help='bin name')
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Process bin name.')
+parser.add_argument('string', type= str, help='bin name')
+args = parser.parse_args()
 
-# ld = cle.Loader(args.string)
-
-ld = cle.Loader("/home/johnny/ptfuzzer/afl-pt/ptest/readelf")
-
-f = open("./raw.txt", "wb")
-bin_code = ""
+ld = cle.Loader(args.string)
 
 entry = ld.main_object.entry
-# 'data', 'header', 'is_null', 'name', 'stream'
 
-sec = ld.main_object.reader.get_section_by_name(".text")
-print sec.header.sh_addr, sec.header.sh_size
-raw_bytes = ld.memory.read_bytes(sec.header.sh_addr, sec.header.sh_size)
-for byte in raw_bytes:
-	bin_code += byte
+#~ sec = ld.main_object.reader.get_section_by_name(".text")
+#~ print sec.header.sh_addr, sec.header.sh_size
+#~ raw_bytes = ld.memory.read_bytes(sec.header.sh_addr, sec.header.sh_size)
+#~ for byte in raw_bytes:
+	#~ bin_code += byte
 
 # for i in ld.main_object.sections:
 # 	if i.name == ".text":
@@ -31,18 +25,18 @@ for byte in raw_bytes:
 # 		for byte in raw_bytes:
 # 			bin_code += byte
 
-print len(bin_code)
-f.write(bin_code)
-f.close()
+#~ print len(bin_code)
+#~ f.write(bin_code)
+#~ f.close()
 
 # print hex(entry)
 
-# min_addr = ld.main_object.min_addr
-# max_addr = ld.main_object.max_addr
+min_addr = ld.main_object.min_addr
+max_addr = ld.main_object.max_addr
 
-# f = open("./min_max.txt", "w")
-# f.write(str(min_addr) + "\n" + str(max_addr) + "\n" + str(entry))
-# f.close()
+f = open("./min_max.txt", "w")
+f.write(str(min_addr) + "\n" + str(max_addr) + "\n" + str(entry))
+f.close()
 
 
 
