75b148c2aad7d969ae9c6591352d7e348929d149
Xu Zhou <zhouxu.cs@outlook.com>
2018-04-03 15:41:05 +0800

    fix some compile error.

--- pt/test_pt.cpp
+++ pt/test_pt.cpp
@@ -39,7 +39,7 @@ bool read_raw_bin()
 {
     FILE* pt_file = fopen("../raw_bin", "rb");
 
-    raw_bin_buf = malloc(max_addr_cle - min_addr_cle);
+    raw_bin_buf = (uint8_t*)malloc(max_addr_cle - min_addr_cle);
     memset(raw_bin_buf, 0, max_addr_cle - min_addr_cle);
 
     if(NULL == pt_file)
@@ -56,12 +56,11 @@ bool read_raw_bin()
     return true;
 }
 
-int main()
+int main(int argc, char** argv)
 {
 	pt_fuzzer fuzzer("raw_bin", 4201040, 4437234, 4203264);
 	fuzzer.init();
 
-	fuzzer.
 
     pid_t pid;        //进程标识符
 	pid = fork();     //创建一个新的进程
@@ -70,13 +69,15 @@ int main()
 		printf("创建进程失败!");
 		exit(1);
 	}
-	else if(pid==0)   //如果pid为0则表示当前执行的是子进程
+	else if(pid == 0)   //如果pid为0则表示当前执行的是子进程
 	{
 		printf("这是子进程,进程标识符是%d\n",getpid());
 		//执行ls
-		printf("execv\n");
 		sleep(1);
-		execv("/home/guy/ptfuzzer/afl-pt/ptest/readelf", "-a /home/guy/ptfuzzer/afl-pt/ptest/in/small_exec.elf");
+        //std::string bin_file = "/home/guy/ptfuzzer/afl-pt/ptest/readelf";
+        //std::string args = "-a /home/guy/ptfuzzer/afl-pt/ptest/in/small_exec.elf";
+        char* args[2] = {"-a", "/home/guy/ptfuzzer/afl-pt/ptest/in/small_exec.elf"};
+		execv("/home/guy/ptfuzzer/afl-pt/ptest/readelf", args);
 		//printf("execv\n");
 		sleep(1);
 		exit(0);
@@ -90,7 +91,6 @@ int main()
 		waitpid(pid, &status, 0);
 		fuzzer.stop_pt_trace();
         printf("\n\n");
-        printf("%d\n", flag);
 	}
 
 	return 0;
