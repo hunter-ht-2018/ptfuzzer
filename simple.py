d53d2392458a526e2ab59e9e74d88110d76490a8
Xu Zhou <zhouxunudt@gmail.com>
2018-05-24 09:02:34 +0800

    refine output.

--- simple.py
+++ simple.py
@@ -7,16 +7,14 @@ f = open(file_name)
 lines = f.readlines()
 filters = ["tip_fup:", "tip:", "tip_pgd:" , "tip_pge:"]
 
-output = []
+
 for line in lines:
     for sf in filters:
         if line.startswith(sf):
-            output.append(line)
+            print line,
             break
 
 f.close()
 
-for l in output:
-    print l ,
 
 
