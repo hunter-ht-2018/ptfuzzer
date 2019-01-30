35a62f89ccaac1f73cd32d5cd8d1cf33cd92f11d
van Hauser <vh@thc.org>
2019-01-30 03:19:50 -0500

    documentation and check for nopti requirement

--- README.md
+++ README.md
@@ -31,6 +31,15 @@ make install
 ```
 This will install all python scripts and binary files to *bin* in the current directory.
 
+
+## Linux kernel versions
+
+A linux kernel of 4.13 or 4.14 is recommended.
+
+If a kernel >= 4.15 is used, the kernel has to be booted with the "nopti" option.
+Beginning with 4.15 page table isolation was introduced to protect against meltdown/spectre attacks which prevents intel_pt to trace a specific process if active.
+
+
 ## How to run
 
 * You need to open the performance switch of the system everytime you reboot the system or simply run **config-run.sh**.
--- config-run.sh
+++ config-run.sh
@@ -1,6 +1,25 @@
+#!/bin/bash
+
+#test "$UID" != 0 && { echo Error: must be run as root ; exit 1 ; }
+
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 sudo modprobe msr
 echo core | sudo tee /proc/sys/kernel/core_pattern
 echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
-
+VER=`uname -r | sed 's/\.[0-9]*-.*//'`
+MAJOR=${VER//.*/}
+MINOR=${VER:2:6}
+case "$MAJOR" in
+  [1-3]) echo Error: kernel too old: $VER ;;
+  [5-9]) echo Warning: unknown kernel version: $VER ;;
+  4)  if [ "$MINOR" -lt 10 ]; then echo Warning: a kernel 4.10 to 4.14 is recommended; else
+         if [ "$MINOR" -gt 14 ]; then
+            BOOT=`dmesg | grep 'BOOT_IMAGE'`
+            if [ -z "$BOOT" ]; then echo Warning: could not determine kernel boot options, ensure you are booting with \"nopti\"; else
+               echo " $BOOT " | grep -qw nopti || echo Error: you must boot your kernel with the \"nopti\" option
+            fi
+         fi
+      fi 
+  *) echo Error: could not determine kernel version: $VER ;;
+esac
